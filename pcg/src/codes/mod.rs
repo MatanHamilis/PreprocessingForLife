9bc0ea83935add9b171f66a6d6b0ec1079f6fc8e
Matan Hamilis <matan.hamilis@gmail.com>
2022-05-22 12:18:54 +0300

    change EAcode API to constant WEIGHT

--- pcg/src/codes/mod.rs
+++ pcg/src/codes/mod.rs
@@ -1,20 +1,17 @@
-use fields::GF128;
 use rand::seq::IteratorRandom;
 use rand::SeedableRng;
 use rand_chacha::ChaCha8Rng;
 
-pub struct EACode {
-    weight: usize,
+pub struct EACode<const WEIGHT: usize> {
     width: usize,
     height: usize,
     cur_height: usize,
     csprng: ChaCha8Rng,
 }
 
-impl EACode {
-    pub fn new(weight: usize, width: usize, height: usize, seed: [u8; 32]) -> Self {
+impl<const WEIGHT: usize> EACode<WEIGHT> {
+    pub fn new(width: usize, height: usize, seed: [u8; 32]) -> Self {
         EACode {
-            weight,
             width,
             height,
             cur_height: 0,
@@ -23,17 +20,15 @@ impl EACode {
     }
 }
 
-impl Iterator for EACode {
-    type Item = Vec<GF128>;
+impl<const WEIGHT: usize> Iterator for EACode<WEIGHT> {
+    type Item = [usize; WEIGHT];
     fn next(&mut self) -> Option<Self::Item> {
         if self.height == self.cur_height {
             return None;
         }
         self.cur_height += 1;
-        let mut output = vec![GF128::zero(); self.width];
-        for i in (0..self.width).choose_multiple(&mut self.csprng, self.weight) {
-            output[i] = GF128::one();
-        }
+        let mut output = [0usize; WEIGHT];
+        (0..self.width).choose_multiple_fill(&mut self.csprng, &mut output);
         Some(output)
     }
 }
@@ -43,7 +38,7 @@ mod tests {
     use crate::codes::EACode;
     #[test]
     pub fn test_sanity() {
-        let code = EACode::new(5, 12, 100, [1; 32]);
+        let code = EACode::<5>::new(12, 100, [1; 32]);
         let mut i = 0;
         for v in code {
             i += 1;
