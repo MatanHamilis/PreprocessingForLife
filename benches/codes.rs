13d1d692a6e379fd052476329154ca6cfac5d48e
Matan Hamilis <matan.hamilis@gmail.com>
2022-09-06 10:24:34 +0300

    add codes bench

---
+++ benches/codes.rs
@@ -0,0 +1,11 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use silent_party::pcg::codes::EACode;
+pub fn bench_codes(c: &mut Criterion) {
+    let mut code = EACode::<8>::new(1 << 17, [0u8; 32]);
+    c.bench_function("bench_codes", |b| {
+        b.iter(|| black_box(code.next().unwrap()))
+    });
+}
+
+criterion_group!(benches, bench_codes);
+criterion_main!(benches);
